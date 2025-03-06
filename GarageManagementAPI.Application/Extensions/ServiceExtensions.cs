9920922eec9e54a9f6b1e0370043ba088cd92a72
BuiKhanh03 <buikhanh0130@gmail.com>
2025-03-06 23:45:39 +0700

    Add Azure Signalr

--- GarageManagementAPI.Application/Extensions/ServiceExtensions.cs
+++ GarageManagementAPI.Application/Extensions/ServiceExtensions.cs
@@ -287,11 +287,7 @@ namespace GarageManagementAPI.Application.Extensions
 
         public static void ConfigureSignalR(this IServiceCollection services)
         {
-            services.AddSignalR(options =>
-            {
-                options.KeepAliveInterval = TimeSpan.FromSeconds(10); 
-                options.ClientTimeoutInterval = TimeSpan.FromSeconds(60); 
-            });
+            services.AddSignalR().AddAzureSignalR();
         }
 
         public static void ConfigureValidator(this IServiceCollection services)
