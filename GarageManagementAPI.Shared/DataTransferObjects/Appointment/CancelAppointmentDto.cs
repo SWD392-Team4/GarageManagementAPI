bcbcf277a01eb1dbaf1016f4c7e60299bdfc68bd
Tan <nightfury455@gmail.com>
2025-03-15 12:21:10 +0700

    fix logic create appointment

---
+++ GarageManagementAPI.Shared/DataTransferObjects/Appointment/CancelAppointmentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace GarageManagementAPI.Shared.DataTransferObjects.Appointment
+{
+    public record CancelAppointmentDto
+    {
+        public string? VerifyCode { get; init; }
+
+        public string? CustomerEmail { get; init; }
+
+        public string? CustomerPhoneNumber { get; init; }
+
+        public DateTimeOffset? EstimatedTime { get; init; }
+    }
+}
