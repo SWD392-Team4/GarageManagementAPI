3e2c9ecced2c19ec355a8e40944bdd8dbede0f22
nightfury455@gmail.com <nightfury455@gmail.com>
2025-03-18 16:25:10 +0700

    Implement interface repository

---
+++ GarageManagementAPI.Repository/CarConditionImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using GarageManagementAPI.Repository.Contracts;
+using GarageManagementAPI.Entities.Models;
+
+namespace GarageManagementAPI.Repository
+{
+    public class CarConditionImageRepository : RepositoryBase<CarConditionImage>, ICarConditionImageRepository
+    {
+        public CarConditionImageRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
